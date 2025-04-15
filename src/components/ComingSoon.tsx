f3d5b25cd46fe7da4b0ca5b70b986d09d81b3f8f
taeke2 <taeke2@naver.com>
2025-04-16 02:57:39 +0900

    feat: Coming Soon 반응형 적용

--- src/components/ComingSoon.tsx
+++ src/components/ComingSoon.tsx
@@ -5,8 +5,8 @@ import Image from "next/image";
 
 export default function ComingSoon({ title }: { title: string }) {
   return (
-    <div className="flex flex-col items-center justify-center mt-[10%] bg-white text-center px-4">
-      <div className="w-24 h-24 mb-12 relative">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-white text-center px-4">
+      <div className="relative w-20 h-20 sm:w-24 sm:h-24 md:w-28 md:h-28 lg:w-32 lg:h-32 mb-12">
         <Image
           src="/circle-check.svg"
           alt="Check Icon"
@@ -15,12 +15,18 @@ export default function ComingSoon({ title }: { title: string }) {
         />
       </div>
 
-      <h1 className="text-5xl font-bold mb-12">{title}</h1>
+      <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold mb-12">
+        {title}
+      </h1>
 
-      <p className="text-4xl mb-3">서비스를 준비중입니다!</p>
-      <p className="text-4xl mb-8">빠른 시일 내에 오픈하겠습니다.</p>
+      <p className="text-xl sm:text-2xl md:text-3xl mb-3">
+        서비스를 준비중입니다!
+      </p>
+      <p className="text-xl sm:text-2xl md:text-3xl mb-8">
+        빠른 시일 내에 오픈하겠습니다.
+      </p>
 
-      <p className="text-4xl">감사합니다.</p>
+      <p className="text-xl sm:text-2xl md:text-3xl">감사합니다.</p>
     </div>
   );
 }
