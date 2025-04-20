bad634de7cf2b1c6bbbb9fb2877ce60619cb1a96
taeke2 <taeke2@naver.com>
2025-04-21 04:39:30 +0900

    fix(Main): Section1 desktop, mobile 레이아웃 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -168,26 +168,31 @@ export default function Home() {
       <section className="relative w-full aspect-video">
         {/* 꽉 찬 배경 이미지 */}
         <Image
-          src="/picture4.svg" // public 폴더에 넣은 이미지 경로
+          src="/picture_main.png" // public 폴더에 넣은 이미지 경로
           alt="Main Background"
           fill
           className="object-cover"
           priority
         />
         {/* 중앙 텍스트 오버레이 */}
-        <div className="absolute inset-0 flex flex-col items-center justify-center text-white text-center px-4">
-          <p className="text-base sm:text-lg md:text-xl lg:text-2xl mb-4">
+        <div className="absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 
+          flex flex-col items-center justify-center text-white">
+          <p className="text-[9px] mb-[5px]
+            lg:text-[29px] lg:mb-[17px]">
             당신의 여정이 머무르지 않도록,
           </p>
-          <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold">
+          <h1 className="font-extrabold text-[21px]
+            lg:text-[61px]">
             우리는 길을 만듭니다
           </h1>
         </div>
 
         {/* 하단 로고 */}
-        <div className="absolute bottom-6 left-1/2 -translate-x-1/2 transform w-24 sm:w-28 md:w-32 lg:w-36 h-8 sm:h-10 md:h-12 lg:h-14">
+        <div className="absolute left-1/2 -translate-x-1/2 transform
+          bottom-[20px] w-[71px] h-[16px]
+          lg:bottom-[37px] lg:w-[175px] lg:h-[40px]">
           <Image
-            src="/logo.png"
+            src="/logo_main_picture.svg"
             alt="Logo-white"
             fill
             className="object-contain"
