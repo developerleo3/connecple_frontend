f2120d54443e5a04a2ca18593fd3ccdb9d20cd5c
developerleo3 <developerleo3@gmail.com>
2025-04-26 02:03:44 +0900

    style(Footer): text size 조정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,7 +11,7 @@ export default function Footer() {
       {/* 메뉴 */}
       <div
         className="flex text-black font-bold
-          text-[8px] gap-x-[11px]
+          text-[7px] gap-x-[11px]
           lg:text-[20px] lg:gap-x-[48px]"
       >
         <Link href="#" className="hover:text-gray-600">
@@ -28,7 +28,7 @@ export default function Footer() {
       <div className="grid grid-cols-5 mt-[3px] lg:mt-[10px]">
         {/* 좌측 정보 (3/5) */}
         <div className="col-span-3 border-r-[1px] border-[#C3C3C3]
-          space-y-[1px] text-[8px]
+          space-y-[1px] text-[7px]
           lg:space-y-[5px] lg:text-[20px]"
         >
           <Image
@@ -39,7 +39,7 @@ export default function Footer() {
             className="lg:w-[272px] lg:h-[61px] mt-[3px]"
           />
           {/* 대표 정보 */}
-          <div className="flex font-medium gap-x-[10px] mt-[3px] lg:mt-[10px] lg:gap-x-[44px]">
+          <div className="flex font-medium gap-x-[10px] mt-[5px] lg:mt-[10px] lg:gap-x-[44px]">
             <p className="font-bold">대표</p>
             <p>박지희</p>
             <p>주식회사커넥플</p>
@@ -62,14 +62,14 @@ export default function Footer() {
 
         {/* 우측 고객센터 (2/5) */}
         <div className="col-span-2 pl-[11px] lg:pl-[112px]">
-          <p className="font-bold text-[8px] lg:text-[20px]">고객센터</p>
-          <p className="font-bold text-[9px] lg:text-[24px]">02-2610-9770<span className="font-medium text-[8px] lg:text-[20px]">  (대표)</span></p>
-          <p className="font-bold text-[9px] lg:text-[24px]">02-2610-9772<span className="font-medium text-[8px] lg:text-[20px]">  (MICE사업부)</span></p>
-          <p className="font-bold text-[9px] lg:text-[24px]">02-2610-9774<span className="font-medium text-[8px] lg:text-[20px]">  (교육사업부)</span></p>
+          <p className="font-bold text-[7px] lg:text-[20px]">고객센터</p>
+          <p className="font-bold text-[8px] lg:text-[24px]">02-2610-9770<span className="font-medium text-[8px] lg:text-[20px]">  (대표)</span></p>
+          <p className="font-bold text-[8px] lg:text-[24px]">02-2610-9772<span className="font-medium text-[8px] lg:text-[20px]">  (MICE사업부)</span></p>
+          <p className="font-bold text-[8px] lg:text-[24px]">02-2610-9774<span className="font-medium text-[8px] lg:text-[20px]">  (교육사업부)</span></p>
           <p className="font-medium text-[6px] lg:text-[20px]">평일 9:00 - 18:00</p>
         </div>
       </div>
-      <p className="text-[8px] mt-[14px] mb-[28px]
+      <p className="text-[6px] mt-[14px] mb-[28px]
         lg:text-[20px] lg:mt-[40px] lg:mb-[70px]">Copyright ⓒ 2025.All right Reserved by CONNECPLE.</p>
     </footer>
   );
