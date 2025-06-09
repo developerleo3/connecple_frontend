5524c2536ca9bb9847b4ef90e7a7c8521b6f9d8b
High-Quality-Coffee <125748258+High-Quality-Coffee@users.noreply.github.com>
2025-06-10 03:22:33 +0900

    feat : 관리자 로그인 페이지 뷰 완성
    - 관리자 로그인 페이지 뷰 완성
    - 로그인 api 연결 안됨

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,20 @@
+'use client'
+
 import '../styles/globals.css'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
+import { usePathname } from 'next/navigation'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname()
+  const isAdminPage = pathname?.startsWith('/admin')
+
   return (
     <html lang="ko">
-      <body>
-        <Header />
+      <body className="__className_e8ce0c">
+        {!isAdminPage && <Header />}
         {children}
-        <Footer />
+        {!isAdminPage && <Footer />}
       </body>
     </html>
   )
