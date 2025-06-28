deba962a51286c9ddc9ad204ca1ada8631fce752
taeke2 <taeke2@naver.com>
2025-06-29 02:47:16 +0900

    feat(withProject): section4 구현

--- src/app/with-project/page.tsx
+++ src/app/with-project/page.tsx
@@ -2,6 +2,8 @@
 
 import Image from "next/image";
 
+import { useState } from "react";
+
 /**
  * font-thin        100
  * font-extralight  200
@@ -80,7 +82,7 @@ export default function WithProjectPage() {
 
       {/* section2 */}
       <section className="w-full h-auto 
-        lg:px-[136px] lg:mt-[300px]">
+        lg:px-[146px] lg:mt-[300px]">
         {/* 상단: 로고 + 문구 */}
         <div className="flex flex-row items-center">
           {/* 왼쪽 로고 */}
@@ -153,7 +155,7 @@ export default function WithProjectPage() {
 
       {/* section3 */}
       <section className="flex flex-col w-full h-auto bg-[#C0AED1B2]
-        lg:px-[136px]"
+        lg:px-[146px]"
         style={{
           background: "linear-gradient(to bottom, rgba(192, 174, 209, 0.7), rgba(255, 255, 255, 1))",
         }}>
@@ -198,6 +200,110 @@ export default function WithProjectPage() {
                   dangerouslySetInnerHTML={{ __html: item.label }}
                 />
               </div>
+            </div>
+          ))}
+        </div>
+      </section>
+
+      {/* section4 */}
+      <section className="flex flex-col w-full h-auto justify-center lg:mt-[83px] lg:px-[146px]">
+        <h1 className="font-black text-[#541E80] text-center lg:text-[45px]">
+          이런분들께 추천합니다.
+        </h1>
+
+        <div className="flex flex-col w-full h-auto lg:mt-[50px]">
+          {[
+            [
+              {
+                picture: "/withProject/recommend1.svg",
+                title: "다시 일하고 싶지만,<br /><span class='font-black'>어디서 시작해야할지<br />막막</span>한 분",
+                content: "경력의 공백이 길수록<br />자신감은 낮아지죠.<br />위드프로젝트는 작지만 확실한<br />첫걸음을 함께 시작합니다."
+              },
+              {
+                picture: "/withProject/recommend2.svg",
+                title: "<span class='font-black'>'엄마'가 아닌 '나'</span><br />로서 다시 일하고<br />싶은 분",
+                content: "육아와 병행하면서도 내 이름으로<br />다시 일하고 싶은 마음,<br />그 소중한 마음을<br />위드프로젝트는 존중합니다."
+              },
+              {
+                picture: "/withProject/recommend3.svg",
+                title: "<span class='font-black'>변화가 빠른 시대</span>에<br />나만 멈춘 것 같아<br />불안한 분",
+                content: "디지털 전환,<br />새로운 기술, 낯선 용어들 ...<br />지금 시작해도 늦지 않아요.<br />우리 함께 따라가볼 수 있습니다."
+              },
+              {
+                picture: "/withProject/recommend4.svg",
+                title: "혼자서는<br /><span class='font-black'>계속 망설이기만</span><br />했던 분",
+                content: "정보는 많지만,<br />누구와 시작해야 할 지<br />몰랐다면 같은 고민을<br />가진 동료들과 함께 도전해요"
+              },
+            ],
+            [
+              {
+                picture: "/withProject/recommend5.svg",
+                title: "<span class='font-black'>재택이나 유연근무</span>가<br />가능한 일자리를<br />원하시는 분",
+                content: "일과 육아의 균형, 이제는<br />선택이 아닌 필수.<br />현실적인 근무환경까지 고려한<br />커리어 방향을 함께 설계해드립니다."
+              },
+              {
+                picture: "/withProject/recommend6.svg",
+                title: "하루 3~5시간은<br /><span class='font-black'>온전히 집중</span>할 수<br />있는 분",
+                content: "짧은 시간이라도 책임감 있게<br />몰입할 준비가 되셨다면,<br />위드프로젝트는 당신의 진심을<br />실력으로 바꿔드립니다."
+              },
+              {
+                picture: "/withProject/recommend7.svg",
+                title: "내 경험이<br /><span class='font-black'>더 이상 쓸모없다</span>고<br />느껴졌던 분",
+                content: "잠시 멈췄을 뿐, 당신의 경력은<br />충분히 다시 쓰일 수 있어요.<br />우리가 함께 그 가치를<br />꺼내드릴게요."
+              },
+              {
+                picture: "/withProject/recommend8.svg",
+                title: "단순한 취업이<br />아닌, <span class='font-black'>나답게</span><br />일하고 싶으신 분",
+                content: "단지 ‘일’을 찾는 게 아니라,<br />나의 삶을 다시 설계하는 여정<br />그 시작을 위드프로젝트가<br />함께합니다."
+              },
+            ],
+          ].map((row, rowIndex) => (
+            <div
+              key={rowIndex}
+              className={`flex flex-row w-full h-auto justify-between ${rowIndex === 1 ? "lg:mt-[39px]" : ""}`}
+            >
+              {row.map((item, idx) => {
+                const isBgGray =
+                  (rowIndex === 0 && idx % 2 === 0) || // 첫째 줄: 짝수
+                  (rowIndex === 1 && idx % 2 === 1);   // 둘째 줄: 홀수
+
+                return (
+                  <div
+                    key={idx}
+                    className={`group flex flex-col items-center
+                      w-[87px] h-[87px] rounded-[12px] shadow-[2px_2px_7px_0_rgba(0,0,0,0.25)]
+                      lg:w-[214px] lg:h-[270px] lg:rounded-[20px]
+                      ${isBgGray ? "bg-white" : "bg-[#F7F7F7]"} hover:bg-[#E2D5F1] transition-all duration-300`}
+                  >
+                    <Image
+                      src={item.picture}
+                      alt={item.title}
+                      width={275}
+                      height={182}
+                      className="group-hover:hidden w-full h-[182px] rounded-tl-[20px] rounded-tr-[20px]
+                        lg:w-[62px] lg:h-[62px] lg:mt-[28px]"
+                    />
+                    <Image
+                      src={"/withProject/recommend_line.svg"}
+                      alt={"recommend_line"}
+                      width={168}
+                      height={3}
+                      className="group-hover:hidden lg:w-full lg:h-auto lg:px-[23px] lg:mt-[23px]"
+                    />
+                    <div className="group-hover:hidden flex w-full h-auto justify-center items-center text-center lg:mt-[30px]">
+                      <p
+                        className="font-bold lg:text-[18px]"
+                        dangerouslySetInnerHTML={{ __html: item.title }}
+                      />
+                    </div>
+                    <div
+                      className="hidden group-hover:flex w-full h-full items-center justify-center text-center font-bold
+                        lg:text-[15px] lg:leading-[22px]"
+                      dangerouslySetInnerHTML={{ __html: item.content }}
+                    />
+                  </div>
+                );
+              })}
             </div>
           ))}
         </div>
