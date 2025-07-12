a9f7c2c42dc61a222c266475c1222ce2484f0b99
taeke2 <taeke2@naver.com>
2025-07-12 11:37:17 +0900

    style(withConnecDay): 모바일 작업 완료

--- src/app/with-connecday/page.tsx
+++ src/app/with-connecday/page.tsx
@@ -54,42 +54,46 @@ export default function WithConnecdayPage() {
                         lg:text-[30px] lg:font-extrabold">
                         W.I.T.H Connecday
                     </h1>
-                    <h1 className="text-center
-                        lg:text-[27px] lg:font-semibold lg:mt-[20px]">
+                    <h1 className="text-center text-[11px] font-bold mt-[25px] lg:text-[27px] lg:font-semibold lg:mt-[20px]">
                         재도약을 꿈꾸는 이 멋진 여성들을 위한 네트워킹 모임
                     </h1>
                 </div>
                 <div className="flex flex-col w-full h-auto justify-center">
                     <h1 className="text-center font-extrabold
-                        text-[13px]
+                        text-[12px] mt-[25px]
                         lg:text-[35px] lg:mt-[92px]">
                         “경력보유여성과 사회를 잇는, 커넥플만의 연결 플랫폼“
                     </h1>
                     <p className="text-center font-semibold
+                        text-[9px] mt-[25px]
                         lg:text-[23px] lg:mt-[77px]">
                         워드커넥데이는 교육 수료생, 참여 기업, 전문가, 참여 동기들과<br/>
                         소통하고 연결하며 새로운 기회를 만드는 네트워킹 행사입니다.
                     </p>
                     <h3 className="text-center font-extrabold
+                        text-[10px] mt-[25px]
                         lg:text-[25px] lg:mt-[62px]">
                         연결을 통해 커리어를 확장하는 날.<br/>
                         <span className="text-[#541E80]">사람과 사회를 잇는 진짜 네트워킹의 장.</span>
                     </h3>
                     <div className="relative flex flex-col items-center">
                         <h1 className="relative z-10 text-center font-tvn-medium text-[#541E80]
-                            lg:text-[35px] lg:mt-[97px]">
+                            text-[13px] mt-[38px] lg:text-[35px] lg:mt-[97px]">
                             선착순 20명 !
                         </h1>
                         <div className="absolute z-0 bg-[#D9D9D9]
+                            w-[63px] h-[9px] mt-[45px]
                             lg:w-[174px] lg:h-[25px] lg:mt-[120px]"/>
                     </div>
                     <Link
                         href="/with-project"
-                        className="bg-[#541E80] text-white flex self-center items-center justify-center font-extrabold
-                        lg:mt-[19px] lg:w-[388px] lg:h-[60px] lg:text-[27px] lg:rounded-[30px]">
+                        className="bg-[#541E80] text-white flex self-center items-center justify-center font-extrabold rounded-[30px]
+                            mt-[9px] w-[159px] h-[25px] text-[10px]
+                            lg:mt-[19px] lg:w-[388px] lg:h-[60px] lg:text-[27px]">
                         나의 성장 가능성 연결하기
                     </Link>
                     <h3 className="flex self-center font-bold
+                        text-[9px] mt-[13px]
                         lg:text-[20px] lg:mt-[25px]">
                         가능성은 연결될 때 빛이 납니다.
                     </h3>
@@ -97,37 +101,41 @@ export default function WithConnecdayPage() {
             </section>
 
             {/* section2 */}
-            <section className="flex flex-col w-full h-auto lg:mt-[88px] lg:px-[123px]">
-                <h1 className="font-black text-[#541E80] lg:text-[45px] lg:mt-[143px]">
+            <section className="flex flex-col w-full h-auto mt-[99px] px-[54px] lg:mt-[88px] lg:px-[123px]">
+                <h1 className="hidden lg:block font-black text-[#541E80] lg:text-[45px] lg:mt-[143px]">
                     한 걸음 내디뎠다면, 이제 연결이 필요한 순간입니다.
                 </h1>
-                <h2 className="font-extrabold lg:text-[27px] lg:mt-[26px]">
+                <h1 className="block lg:hidden font-black text-[#541E80] text-[15px]">
+                    한 걸음 내디뎠다면,<br/> 이제 연결이 필요한 순간입니다.
+                </h1>
+                <h2 className="font-black lg:font-extrabold text-[10px] mt-[9px] lg:text-[27px] lg:mt-[26px]">
                     같은 길을 걷는 동료들, 실무에서 찾는 기업들,<br/>
                     내 가능성을 믿어주는 멘토들과 서로 연결되는 단 하루의 기회!
                 </h2>
-                <div className="flex flex-row justify-between lg:mt-[64px]">
-                    <div className="group flex justify-center items-center rounded-full bg-[#F6F6F6] shadow-[4px_4px_4px_0_rgba(0,0,0,0.25)]
-                        lg:w-[296px] lg:h-[296px] hover:bg-[#2F0D4B] transition-all duration-300">
-                        <div className="text-center">
+                <div className="flex flex-row justify-between mt-[27px] lg:mt-[64px]">
+                    <div className="group flex justify-center items-center rounded-full bg-[#F6F6F6] transition-all duration-300
+                        w-[85px] h-[85px] shadow-[1px_1px_3.7px_0_rgba(0,0,0,0.11)]
+                        lg:w-[296px] lg:h-[296px] lg:hover:bg-[#2F0D4B] lg:shadow-[4px_4px_4px_0_rgba(0,0,0,0.25)]">
+                        <div className="text-center pt-[5px]">
                             {/* 일반 상태 */}
-                            <p className="font-bold lg:text-[27px] leading-relaxed group-hover:hidden">
+                            <p className="font-bold text-[10px] lg:text-[27px] leading-relaxed lg:group-hover:hidden">
                                 위드프로젝트<br/>설명회<br/>
                                 <span className="inline-flex items-center justify-center">
                                     <Image
                                         src="/withConnecDay/triangle_bottom.svg"
                                         alt="triangle_bottom"
-                                        width={27}
-                                        height={27}
+                                        width={6}
+                                        height={6}
                                         unoptimized
-                                        className="inline-block align-middle"
+                                        className="inline-block align-middle lg:w-[27px] lg:h-[27px]"
                                     />
                                 </span>
                                 <br/>
-                                <span className="font-extrabold">정보</span>
+                                <span className="font-extrabold text-[#541E80] lg:text-black">정보</span>
                             </p>
 
                             {/* 호버 상태 */}
-                            <div className="hidden group-hover:flex flex-col items-center text-white">
+                            <div className="hidden lg:group-hover:flex flex-col items-center text-white">
                                 <p className="font-black lg:text-[20px]">Check Point</p>
                                 <div className="border-[1px] border-white lg:w-[40px] lg:my-[13px]"/>
                                 <p className="lg:text-[18px] font-semibold">
@@ -141,25 +149,26 @@ export default function WithConnecdayPage() {
                         </div>
                     </div>
 
-                    <div className="group flex justify-center items-center rounded-full bg-[#ECECEC] shadow-[4px_4px_4px_0_rgba(0,0,0,0.25)]
-                        lg:w-[296px] lg:h-[296px] hover:bg-[#2F0D4B] transition-all duration-300">
-                        <div className="text-center">
-                            <p className="font-bold text-center lg:text-[27px] leading-relaxed group-hover:hidden">
+                    <div className="group flex justify-center items-center rounded-full bg-[#ECECEC] transition-all duration-300
+                        w-[85px] h-[85px] shadow-[1px_1px_3.7px_0_rgba(0,0,0,0.11)]
+                        lg:w-[296px] lg:h-[296px] lg:hover:bg-[#2F0D4B] lg:shadow-[4px_4px_4px_0_rgba(0,0,0,0.25)]">
+                        <div className="text-center pt-[5px]">
+                            <p className="font-bold text-[10px] lg:text-[27px] leading-relaxed lg:group-hover:hidden">
                                 인사이트<br/>특강<br/>
                                 <span className="inline-flex items-center justify-center">
                                 <Image
                                     src="/withConnecDay/triangle_bottom.svg"
                                     alt="triangle_bottom"
-                                    width={27}
-                                    height={27}
+                                    width={6}
+                                    height={6}
                                     unoptimized
-                                    className="inline-block align-middle"
+                                    className="inline-block align-middle lg:w-[27px] lg:h-[27px]"
                                 />
                                 </span>
                                 <br/>
-                                <span className="font-extrabold">동기부여</span>
+                                <span className="font-extrabold text-[#541E80] lg:text-black">동기부여</span>
                             </p>
-                            <div className="hidden group-hover:flex flex-col items-center text-white">
+                            <div className="hidden lg:group-hover:flex flex-col items-center text-white">
                                 <p className="font-black lg:text-[20px]">Check Point</p>
                                 <div className="border-[1px] border-white lg:w-[40px] lg:my-[13px]"/>
                                 <p className="lg:text-[18px] font-semibold">
@@ -172,25 +181,26 @@ export default function WithConnecdayPage() {
                         </div>
                     </div>
 
-                    <div className="group flex justify-center items-center rounded-full bg-[#F6F6F6] shadow-[4px_4px_4px_0_rgba(0,0,0,0.25)]
-                        lg:w-[296px] lg:h-[296px] hover:bg-[#2F0D4B] transition-all duration-300">
+                    <div className="group flex justify-center items-center rounded-full bg-[#F6F6F6] transition-all duration-300
+                        w-[85px] h-[85px] shadow-[1px_1px_3.7px_0_rgba(0,0,0,0.11)]
+                        lg:w-[296px] lg:h-[296px] lg:hover:bg-[#2F0D4B] lg:shadow-[4px_4px_4px_0_rgba(0,0,0,0.25)]">
                         <div className="text-center">
-                            <p className="font-bold text-center lg:text-[27px] leading-relaxed group-hover:hidden">
+                            <p className="font-bold text-[10px] lg:text-[27px] leading-relaxed lg:group-hover:hidden">
                                 네트워킹<br/>티타임<br/>
                                 <span className="inline-flex items-center justify-center">
                                 <Image
                                     src="/withConnecDay/triangle_bottom.svg"
                                     alt="triangle_bottom"
-                                    width={27}
-                                    height={27}
+                                    width={6}
+                                    height={6}
                                     unoptimized
-                                    className="inline-block align-middle"
+                                    className="inline-block align-middle lg:w-[27px] lg:h-[27px]"
                                 />
                                 </span>
                                 <br/>
-                                <span className="font-extrabold">관계 형성</span>
+                                <span className="font-extrabold text-[#541E80] lg:text-black">관계 형성</span>
                             </p>
-                            <div className="hidden group-hover:flex flex-col items-center text-white">
+                            <div className="hidden lg:group-hover:flex flex-col items-center text-white">
                                 <p className="font-black lg:text-[20px]">Check Point</p>
                                 <div className="border-[1px] border-white lg:w-[40px] lg:my-[13px]"/>
                                 <p className="lg:text-[18px] font-semibold">
@@ -203,52 +213,87 @@ export default function WithConnecdayPage() {
                         </div>
                     </div>
                 </div>
+                <div className="flex lg:hidden flex-row justify-between mt-[27px] lg:mt-[64px]">
+                    {[
+                        "전문가의 생생한 이야기와<br/>" +
+                        "실무 중심의 내용을 통해<br/>" +
+                        "현장에서 바로 활용할 수<br/>" +
+                        "있는 <span className=\"font-black]\">깊이 있는 정보</span>를<br/>" +
+                        "얻을 수 있어요!",
+
+                        "멘토들의 진솔한 경험을<br/>" +
+                        "통해 <span className=\"font-black\">나만의 가능성과<br/>" +
+                        "열정</span>을 다시 떠올리게 되는<br/>" +
+                        "소중한 시간이 될거에요!<br/><br/>",
+
+                        "다양한 사람들과의<br/>" +
+                        "<span className=\"font-black text-[#C0AED1]\">편안한 소통</span> 속에서 공감과<br/>" +
+                        "자극을 얻으며 자연스럽게<br/>" +
+                        "<span className=\"font-black text-[#C0AED1]\">동기부여</span>가 생겨요!<br/><br/>"
+                    ].map((item, idx) => (
+                        <div key={idx}
+                            className="flex justify-center items-center rounded-full bg-white border-[0.7px] border-[#541E80]
+                                w-[85px] h-[85px]">
+                            <div className="flex flex-col items-center stext-white">
+                                <p className="font-bold text-[7px]">Check Point</p>
+                                <div className="border-[0.5px] border-[#541E80] w-[15px] my-[7px]"/>
+                                <p className="text-[5px] font-bold text-center"
+                                    dangerouslySetInnerHTML={{ __html: item }}>
+
+                                </p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </section>
 
             {/* section3 - 버튼 클릭에 따라 콘텐츠 변경 */}
-            <section className="flex flex-row w-full justify-between h-auto lg:px-[130px] lg:py-[90px] lg:mt-[300px] lg:mb-[157px]">
+            <section className="flex flex-row w-full justify-between h-auto
+                    px-[45px] mt-[88px] mb-[84px]
+                    lg:px-[130px] lg:py-[90px] lg:mt-[300px] lg:mb-[157px]">
                 <div className="flex flex-col">
-                    <h1 className="font-black text-[#541E80] lg:text-[45px]">PROGRAM</h1>
-                    <div className="flex flex-row lg:mt-[38px]">
+                    <h1 className="font-black text-[#541E80] text-[15px] lg:text-[45px]">PROGRAM</h1>
+                    <div className="flex flex-row mt-[27px] lg:mt-[38px]">
                         {[1, 2, 3].map((num, idx) => (
                             <button
                                 key={num}
                                 onClick={() => setSelected(idx)}
-                                className={`flex justify-center items-center font-extrabold rounded-full lg:text-[30px] lg:w-[50px] lg:h-[50px] ${
-                                    selected === idx ? "bg-[#541E80] text-white" : "bg-[#D9D9D9] cursor-pointer"
-                                } ${idx !== 0 ? "lg:ml-[17px]" : ""}`}
+                                className={`flex justify-center items-center font-extrabold rounded-full 
+                                    text-[8px] w-[20px] h-[20px] lg:text-[30px] lg:w-[50px] lg:h-[50px] 
+                                    ${selected === idx ? "bg-[#541E80] text-white" : "bg-[#D9D9D9] cursor-pointer"} 
+                                    ${idx !== 0 ? "ml-[10px] lg:ml-[17px]" : ""}`}
                             >
                                 {num}
                             </button>
                         ))}
                     </div>
 
-                    <h2 className="font-extrabold lg:text-[32px] lg:mt-[46px]">{current.title}</h2>
-                    <h3 className="font-bold lg:text-[28px]">: {current.subTitle}</h3>
-                    <h2 className="font-tvn-medium text-[#541E80] lg:text-[35px] lg:mt-[16px]">
+                    <h2 className="font-extrabold text-[10px] mt-[15px] lg:text-[32px] lg:mt-[46px]">{current.title}</h2>
+                    <h3 className="font-bold text-[9px] lg:text-[28px]">: {current.subTitle}</h3>
+                    <h2 className="font-tvn-medium text-[#541E80] text-[15px] mt-[6px] lg:text-[35px] lg:mt-[16px]">
                         {current.slogun}
                     </h2>
 
-                    <div className="lg:w-[446px] border-t-3 border-dotted lg:mt-[17px]"/>
+                    <div className="w-[189px] lg:w-[446px] border-t-1 border-dotted mt-[10px] lg:mt-[17px]"/>
 
                     <p
-                        className="font-extrabold lg:text-[19px] lg:mt-[29px]"
+                        className="font-extrabold text-[7.8px] mt-[7px] lg:text-[19px] lg:mt-[29px]"
                         dangerouslySetInnerHTML={{__html: current.content}}
                     />
 
-                    <div className="lg:w-[446px] border-t-3 border-dotted lg:mt-[29px]"/>
+                    <div className="w-[189px] lg:w-[446px] border-t-1 border-dotted mt-[18px] lg:mt-[17px]"/>
 
                     {[current.comment1, current.comment2, current.comment3].map((cmt, i) => (
-                        <div key={i} className="flex flex-row items-center lg:mt-[15px]">
+                        <div key={i} className="flex flex-row items-center mt-[6px] lg:mt-[15px]">
                             <Image
                                 src="/withConnecDay/check_purple.png"
                                 alt="check_purple.png"
-                                width={25}
-                                height={25}
+                                width={8}
+                                height={7}
                                 unoptimized
                                 className="lg:w-[25px] lg:h-[25px]"
                             />
-                            <p className="font-bold text-[#541E80] lg:text-[18px] lg:ml-[10px]">{cmt}</p>
+                            <p className="font-semibold lg:font-bold text-[#541E80] text-[8px] ml-[4px] lg:text-[18px] lg:ml-[10px]">{cmt}</p>
                         </div>
                     ))}
                 </div>
@@ -256,10 +301,10 @@ export default function WithConnecdayPage() {
                 <Image
                     src={current.image_path}
                     alt={current.title}
-                    width={514}
-                    height={636}
+                    width={124}
+                    height={195}
                     unoptimized
-                    className="object-cover lg:w-[514px] lg:h-[636px] lg:rounded-[30px]"
+                    className="object-cover rounded-[20px] lg:w-[514px] lg:h-[636px] lg:rounded-[30px]"
                 />
             </section>
         </main>
